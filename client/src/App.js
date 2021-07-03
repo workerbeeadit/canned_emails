2d6cc67cda19e6462b4ff3800b493f07d0a7f29e
Adit Daga <adit.daga@Starman.local>
2021-07-03 13:44:46 -0400

    A simple static canned emails with one template

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios'
+// Used https://daveceddia.com/react-image-tag/ to insert an image as a React Component
+import dilbert from './canned-emails-frontliner.jpg';
 
 class App extends Component {
   state = {
@@ -18,8 +20,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Frontend ok!</h1>
-        <h1>{this.state.response.body}</h1>
+        <h1>Canned Emails </h1>
+        <h2> Stop rewriting emails</h2> 
+        <img src = {dilbert} alt = "Everyone has humble beginnings and thus a shrine to daddy elon"/>
+        <h2> Asking for an intro </h2>
+        <h3> Subject: [Intro to Person B] </h3>
+        <p> Hi [Person you want the intro from], I'm writing because I was wondering you could connect me with [Person B]?
+        Specifically, I'd like to know more about [what you'd like to get out of the interaction with Person B]. [Person B] seems like a great fit.
+        Let me know, no worries either way
+        [Sender]</p>
+
+
       </div>
     );
   }
